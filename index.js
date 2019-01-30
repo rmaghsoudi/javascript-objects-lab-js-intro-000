e318c57d8eb674eeefab3e2bf1d1939450023689
Romy Maghsoudi <romy_mag@outlook.com>
2019-01-30 00:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {prop : 1}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, recipes, {prop1: 2})
+  return Object.assign({}, recipes, {prop2: 2})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
