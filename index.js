d550068b39f5353262f88eeb3bb20faa1a5fd9e5
Romy Maghsoudi <romy_mag@outlook.com>
2019-01-30 00:00:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes.prop2 = "2"
+  recipes.prop2 = 2
   return recipes
 }
 
